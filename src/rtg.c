a7fd529f6ead7083a24895837d1bada42d1c0679
Adrien Nini Pereira <adrien97nini@gmail.com>
2017-10-19 18:08:45 +0200

    Using MsgQueue to store value for next seed

--- src/rtg.c
+++ src/rtg.c
@@ -2,26 +2,60 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
 
-
+// RANDOM VARs
 int randTime;
-int randPool[20][5];
+int randPool[20];
+
+// SHARED MEMORY VARs
+key_t key = 77;
+int msgqid;
+int type = 1;
+int d;
+
+struct msg {
+	long type;
+	int data;
+};
+
 
 int main (int argc, char *argv[]) {
 
-	srand((unsigned)time(NULL));
-	int a, b;
-	for (a = 0; a < 20; a++){
-		for (b = 0; b < 5; b++){
-			randTime = rand() % 30;
-			randPool[a][b] = randTime;
-		}
+	// CREATE A SHARED MEMORY (MSGQUEUE)
+	if ((msgqid = msgget(key, 0666|IPC_CREAT)) == -1) {
+		perror("msgget: msgget failed");
+		exit(1);
+	} else {
+		printf("MsqgQueue: Creation succeded !\n");
+	} 
+
+	// CHECK IF SOMETHING STORED IN MEMORY
+	if ((msgrcv(msgqid, &d, sizeof(d), type, IPC_NOWAIT)) == -1) {
+		printf("no data in memory\n");
+	} else {
+		printf("Data : %d\n", d);
 	}
 
-	printf("%d\n", randPool[rand() % 20][rand() % 5]);
+	
+	srand((unsigned)time(NULL));
+	int a, b;
+	for (a = 0; a < 20; a++) {
+		randTime = rand() % 30;
+		randPool[a] = randTime;
+		printf("%d\n", randTime);
+	}
+
+	struct msg m;
+	m.type = type;
+	m.data = 123456;
+
+	printf("%d\n", m.data);
+	msgsnd(msgqid, &m, sizeof(m.data), 0);
+
+	//printf("%d\n", randPool[rand() % 20];
 
 
-	return 1;
- 
-
+	return 0;
 }
