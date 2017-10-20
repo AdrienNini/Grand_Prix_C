e2e9ce4bf22b050a493d4a3d49fc1d9b4cf74ea0
Adrien Nini Pereira <adrien97nini@gmail.com>
2017-10-20 09:51:26 +0200

    Implementation of Shared Memory

--- src/rtg.c
+++ src/rtg.c
@@ -1,5 +1,6 @@
 //RANDOM TIME GENERATOR
 #include <stdio.h>
+#include <unistd.h>
 #include <time.h>
 #include <stdlib.h>
 #include <sys/ipc.h>
@@ -10,31 +11,39 @@ int randTime;
 int randPool[20];
 
 // SHARED MEMORY VARs
-key_t key = 77;
+key_t key = 5678;
 int shmid;
-char *shmaddr;
-int size = 1;
+char *shmaddr, *s;
+int size = sizeof(int);
 
+
+// VARIABLES
+int randSeed;
 
 
 int main (int argc, char *argv[]) {
 
 	// CREATE A SHARED MEMORY
 	initSH();	
-	
-	// CHECK IF SOMETHING STORED IN MEMORY
-	attachSh();	
+	attachSH(shmid);
 
+	// WRITE SOMETHING IN MEMORY
+	s = shmaddr;
+	*s = 5;	
+
+	// READ SOMETHING FROM SHARED MEMORY
+	printf("%d\n", *shmaddr);
+	
 
 	srand((unsigned)time(NULL));
 	int a, b;
 	for (a = 0; a < 20; a++) {
-		randTime = rand() % 30;
+		randTime = rand() % 35;
 		randPool[a] = randTime;
 		printf("%d\n", randTime);
 	}
 
-
+	
 	//printf("%d\n", randPool[rand() % 20];
 
 
@@ -42,7 +51,7 @@ int main (int argc, char *argv[]) {
 }
 
 int initSH() {
-	if ((shmid = shmget(key, size, IPC_CREAT)) == -1) {
+	if ((shmid = shmget(key, size, IPC_CREAT|0660)) == -1) {
 		perror("shmget: shmget failed"); 
 		exit(1);
 	} else {
@@ -51,8 +60,16 @@ int initSH() {
 	}
 }
 
-int attachSh() {
-	shmaddr = shmat(shmid, NULL, 0); 
-	return 0;
+int attachSH(int id) {
+	if ((shmaddr = shmat(id, NULL, 0)) == (char *)  -1) {
+		perror("shmat: shmat failed");
+		exit(1);
+	} else {
+		printf("shmat: shmat succeded !\n");
+		return 0;
+	}
 }
 
+int writeSH(int data) {
+		
+}
