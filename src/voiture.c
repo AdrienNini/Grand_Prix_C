7a58c0420a44e73a9c06a3f2ec808714814f227f
adrien <adrien@localhost.localdomain>
2017-11-15 15:08:43 +0100

    Create voiture process and pseudo code

---
+++ src/voiture.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+struct car {
+	int id;
+	int pitFlag;
+	int outFlag;
+		
+};
+
+int main (int argc, char *argv[]) {
+	
+	// Loop while !raceisOver
+	//
+	// Generate first sector time 
+	//
+	// Generate second sector time
+	//
+	// Generate third sector time
+	// isPitStop ? 
+
+}
+
+
+void writeSectorTime() {
+	// Write the best sector time in the Shared Memory
+}
+
+void checkBestSectorTime() {
+	// Check if the new sector time is better than the last best one (general)
+}
+
+void calcLap() {
+	// Calc the total lap time
+}
+
+void writeLapTime() {
+	// Write the lap time in the Shared Memory
+}
+
+int getTime() {
+	// Call the time generator function
+}
