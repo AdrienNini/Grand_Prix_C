99e63496db7c4c86ce4291ef44a622b4593657ef
adrien <adrien@localhost.localdomain>
2017-11-29 15:20:28 +0100

    debug

--- src/utils.h
+++ src/utils.h
@@ -1,25 +1,32 @@
 struct car {
          int id;
-         bool pitFlag;
-         bool  crashed;
-         int[3] sectorsTime;
+         int pitFlag;
+         int  crashed;
+         int sectorsTime[3];
          int lapTime;
          int pitTime;
          int raceTime;
 };
 
-bool mountSHM() {
+// SHARED MEMORY
+
+key_t key = 5678;
+int shmid;
+char *shmaddr;
+int size = sizeof(struct car) * 20;
+
+int mountSHM() {
         // Init Shared Memory
         if ((shmid = shmget(key, size, IPC_CREAT|0660)) == -1) {
                 perror("shmget: shmget failed");
-                return false;
+                return -1;
         } else {
                 // Attach SHM
                 if ((shmaddr = shmat(shmid, NULL, 0)) == (char *) -1) {
                         perror("shmat: shmat failed");
-                        return false;
+                        return -1;
                 } else {
-                        return true;
+                        return 0;
                 }
         }
 }
