c6f469f84aa57b6059f0797f250835485a874e7d
root <root@localhost.localdomain>
2017-12-13 15:03:28 +0100

    Create circuit file

--- src/circui.c
+++ src/circui.c
@@ -1,29 +1,31 @@
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/shm.h>
 #include "utils.h"
 
 
-key_T key = 5678;
 
 
-int main (int argc, char* []argv){
+int main (int argc, char* argv[]){
 	int i;
 	int pidF;
 	// mount the share memory
-	if(mountSHM = -1){
+	if(mountSHM() == -1){
 		perror("Error init share memory in race");
 	}
 	
 	//creat the cars
-	for(i = 0; i < 1; i++){
-		if((pidF = for()) == -1){
+	for(i = 0; i < 20; i++){
+		if((pidF = fork()) == -1){
 			perror("Error fork");
 		}
 		if(pidF != 0){
-			if(execv(voiture.c, i + '0') == -1){
+			if(execv("../bin/voiture", i + '0') == -1){
 				perror("Error exec car");
 			}
 		}
-	
+		
 	}
+	
 } 
